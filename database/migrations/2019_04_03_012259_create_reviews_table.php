e4dc0d9425ed7c965c4cad459daaae1850c512d8
syifa nurinayah <syifanurinayah07@gmail.com>
2019-04-02 19:04:49 -0700

    DB also added for migration

--- database/migrations/2019_04_03_012259_create_reviews_table.php
+++ database/migrations/2019_04_03_012259_create_reviews_table.php
@@ -15,7 +15,13 @@ class CreateReviewsTable extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('product_id')->unsigned()->index();
+            $table->srting('customer');
+            $table->text('review');
+            $table->integer('star');
             $table->timestamps();
+
+            $table->foreign('product_id')->references('id')->on('product')->onDelete('cascade');
         });
     }
 
