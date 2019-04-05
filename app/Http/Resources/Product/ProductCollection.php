f8a8d152b9b2cd65e848b85d70e2c277540ebed1
syifa nurinayah <syifanurinayah07@gmail.com>
2019-04-04 23:09:24 -0700

    products collection resource

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Resources\Product;
 
-use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Resources\Json\Resource;
 
-class ProductCollection extends ResourceCollection
+class ProductCollection extends Resource
 {
     
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'name' => $this->name,
+            'toalPrice' => round($this->price - ($this->price * $this->discount/100),2),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No rating yet',//reviews di ambil dari model product
+            'discount' => $this->discount, 
+            'href' => [
+                'link' => route('products.show',$this->id)
+            ]
+        ];
     }
 }
