cd5a3ff32ef611e5baef53aee3f29669b6b1adb4
syifa nurinayah <syifanurinayah07@gmail.com>
2019-04-10 19:19:04 -0700

    product update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -66,7 +66,12 @@ class ProductController extends Controller
     
     public function update(Request $request, Product $product)
     {
-        //
+        $request['detail'] = $request->description;
+        unset($request['description']);
+        $product->update($request->all());
+        return response([
+            'data' => new ProductResource($product)
+        ],Response::HTTP_CREATED);
     }
 
     public function destroy(Product $product)
