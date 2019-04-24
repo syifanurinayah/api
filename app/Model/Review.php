9472a186d926fdad021190a35feb751d18e369d8
syifa nurinayah <syifanurinayah07@gmail.com>
2019-04-23 18:47:40 -0700

    reviews created

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
+    protected $table = 'reviews';
+    protected $fillable = [
+        'customer',
+        'star',
+        'review'
+    ];
+
     public function products(){
         return $this->belongsTo(Product::class);
     }
